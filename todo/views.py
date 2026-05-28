6068fb6a35b3b67710f177f789bd262dd3b907a0
Rahul Kumar <84038547+RahulK847@users.noreply.github.com>
2026-05-28 23:11:13 +0530

    Display tasks on home page
    Connect the Task model to the home view and render a task list in the template. Imported Task and queried Task.objects.all(), replaced the previous 'name' context with 'tasks', and updated templates/home.html to iterate over tasks and display title, user, created_at, deadline, and completed fields.

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 
@@ -8,7 +9,8 @@ def base(request):
         'naam': 'Django'})
 
 def home(request):
+    tasks = Task.objects.all()
     context = {
-        'name': 'Rahul',
+        'tasks': tasks
     }
     return render(request, 'home.html', context)
