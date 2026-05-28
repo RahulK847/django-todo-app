a2d0e1418e1a643b85134c7938ba853442992512
Rahul Kumar <84038547+RahulK847@users.noreply.github.com>
2026-05-28 18:11:17 +0530

    Implemented Task model and database migrations

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+admin.site.site_header = "Todo App Admin"
+admin.site.site_title = "Todo App Admin Portal"
+admin.site.index_title = "Welcome to the Todo App Admin Portal"
+admin.site.register(Task)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    priority_choices = [
+        (0, 'High'),
+        (1, 'Normal'),
+        (2, 'Low'),
+    ]
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    priority = models.IntegerField(default=1, choices=priority_choices)
+    deadline = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
